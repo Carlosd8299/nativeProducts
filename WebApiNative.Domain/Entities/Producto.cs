59e324544506907c76fd070326b56fa3570af0a2
Carlos Andres De La Cruz Rodriguez <Carlos_Delacruz@satrack.com>
2024-05-16 15:10:08 -0500

    Agregar archivos de proyecto.

---
+++ WebApiNative.Domain/Entities/Producto.cs
@@ -0,0 +1,40 @@
+﻿namespace WebApiNative.Domain.Entities
+{
+    public class Producto
+    {
+        public Guid Id { get; private set; }
+        public string Nombre { get; private set; }
+        public string Descripcion { get; private set; }
+        public string Categoria { get; private set; }
+        public double Precio { get; private set; }
+        public long CantidadInicial { get; private set; }
+
+        public Producto(Guid id, string nombre, string descripcion, string categoria, double precio, long cantidadInicial)
+        {
+            Id = id;
+            Nombre = nombre;
+            Descripcion = descripcion;
+            Categoria = categoria;
+            Precio = precio;
+            CantidadInicial = cantidadInicial;
+        }
+
+        public Producto()
+        {
+        }
+
+        public Producto ActualizarProducto(long cantidad, double precio, string categoria, string descripcion)
+        {
+            Producto producto = new Producto();
+
+            producto.CantidadInicial = cantidad;
+            producto.Precio = precio;
+            producto.Categoria = categoria;
+            producto.Descripcion = descripcion;
+
+            return producto;
+        }
+
+
+    }
+}
---
+++ src/WebApiNative.Infraestructure/NativeDBContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiNative.Domain.Entities;
+
+namespace WebApiNative.Infraestructure
+{
+    public class NativeDBContext : DbContext
+    {
+        public DbSet<Producto> Productos { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("NativeProductDatabase");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Insertar registros iniciales
+            modelBuilder.Entity<Producto>().HasData(
+                new Producto(Guid.NewGuid(), "Medias", "Media Azul", "Ropa Interior", 12.80, 100),
+                new Producto(Guid.NewGuid(), "Medias", "Media Roja", "Ropa Interior", 12.80, 100),
+                new Producto(Guid.NewGuid(), "Jeans Cargo", "Tipo militar", "Prendas Superiores", 120, 100),
+                new Producto(Guid.NewGuid(), "Jeans Oversized", "Unisex", "Prendas Superiores", 120, 1000),
+                new Producto(Guid.NewGuid(), "FaldaShort", "FaldaShort Azul", "Prendas Superiores", 30, 1000),
+                new Producto(Guid.NewGuid(), "Vestido", "Vestido Azul", "Prenda Liviana", 50, 1000)
+            );
+        }
+
+    }
+}
